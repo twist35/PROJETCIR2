22694a6bbb3c7fd776bd41ec08590bdd90bb0b0b
Lucas-Le-Bihan <71370043+Lucas-Le-Bihan@users.noreply.github.com>
2022-06-16 16:59:03 +0200

    requetes sql

---
+++ code/sql/adduser.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE projetcir2 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE
+utf8_general_ci;
+CREATE USER 'user1'@'localhost' IDENTIFIED BY 'isen29';
+GRANT ALL PRIVILEGES ON projetcir2.* TO 'user1'@'localhost' WITH GRANT OPTION;
