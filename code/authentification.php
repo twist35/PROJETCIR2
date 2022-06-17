7459676efe96752156f8a94905a5223edbc91b3a
twist35 <sabironantonin@outlook.fr>
2022-06-17 09:31:04 +0200

    page authentification ok

--- code/authentification.php
+++ code/authentification.php
@@ -8,14 +8,17 @@
 <div class="titre-page back-b-marine text-center h2 text-marron py-1">
     Authentification
 </div>
-<form>
-  <div class="form-group text-center">
+<form class="text-center center d-flex justify-content-center flex-column align-items-center">
+  <div>Se connecter : </div>
+  <div class="form-group vw-60 mb-2"> 
+    <label for="login"></label>
+    <small class="form-text text-muted  text-b-marine">Email</small>
+    <input type="text" class="form-control " id="login" placeholder="Entrer votre email">
+  </div>
   <div class="form-group vw-60 mb-2">
-    <div>Mot de passe</div>
-    <label for="mdp"></label>
-    <small class="form-text text-muted  text-b-marine">Modifier votre mot de passe :</small>
-    <input type="text" class="form-control " id="ville" placeholder="Entrer l'ancien mot de passe">
-    <input type="text" class="form-control " id="ville" placeholder="Entrer le nouveau mot de passe">
+  <label for="mdp"></label>
+    <small class="form-text text-muted  text-b-marine">Mot de passe</small>
+    <input type="text" class="form-control" id="mdp" placeholder="Entrer votre mot de passe">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
