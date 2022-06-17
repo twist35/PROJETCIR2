22cc8db61174e740c6be416200b9d0a49fc83080
twist35 <sabironantonin@outlook.fr>
2022-06-17 09:20:57 +0200

    deb authentification

--- code/authentification.php
+++ code/authentification.php
@@ -10,17 +10,12 @@
 </div>
 <form>
   <div class="form-group text-center">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  <div class="form-group vw-60 mb-2">
+    <div>Mot de passe</div>
+    <label for="mdp"></label>
+    <small class="form-text text-muted  text-b-marine">Modifier votre mot de passe :</small>
+    <input type="text" class="form-control " id="ville" placeholder="Entrer l'ancien mot de passe">
+    <input type="text" class="form-control " id="ville" placeholder="Entrer le nouveau mot de passe">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
