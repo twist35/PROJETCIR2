cf85df59327258cf763717f6c55d6b61e1042432
Lucas-Le-Bihan <71370043+Lucas-Le-Bihan@users.noreply.github.com>
2022-06-21 11:39:51 +0200

    filtre

--- code/js/display.js
+++ code/js/display.js
@@ -5,6 +5,16 @@ ajaxRequest('GET', 'php/requete.php/mesmatchOrganisateur/', displayMesMatchOrga)
 ajaxRequest('GET', 'php/requete.php/mesmatchParticipant/', displayMesMatchParti);
 ajaxRequest('GET', 'php/requete.php/lesmatch/', displayLesMatch);
 
+$('#formulaire-recherche').submit((event) =>
+  {
+    console.log('appuyé');
+    event.preventDefault();
+    ajaxRequest('POST', 'php/requete.php/filtreMesMatch/', filtreMesMatch,'sport=' + $('#sport').val() + '&date=' + $('#date_r').val() + '&ville=' + $('#ville').val() + '&dispo=' + $('#dispo').val()
+    );
+  }
+);
+
+
 function displayMesMatchOrga(matchs){
     console.log(matchs);
     for(let match of matchs){
@@ -26,6 +36,9 @@ function displayMesMatchOrga(matchs){
                                 '<div>Places : <span id="places">' + match.places_restantes + '</span></div>' +
                                 '<div>Joueurs inscrits : <span id="inscrits">' + match.nb_joueurs + '</span></div>' +
                                 '</div>' +
+                                '</div>' +
+                                '<div class="h6 text-marron">' +
+                                '<span id="joueur-orga">Organisateur</span>' +
                                 '</div>' +
                                 '</div>'
                                 );
@@ -53,6 +66,39 @@ function displayMesMatchParti(matchs){
                                 '<div>Places : <span id="places">' + match.places_restantes + '</span></div>' +
                                 '<div>Joueurs inscrits : <span id="inscrits">' + match.nb_joueurs + '</span></div>' +
                                 '</div>' +
+                                '</div>' +
+                                '<div class="h6 text-marron">' +
+                                '<span id="joueur-orga">Joueur</span>' +
+                                '</div>' +
+                                '</div>'
+                                );
+    }
+}
+
+function filtreMesMatch(matchs){
+    console.log(matchs);
+    for(let match of matchs){
+        //console.log(match);
+        //console.log(match.nom_partie)
+        $('#mes_matchs').append('<div class="bulle text-center">' +
+                                '<div class="d-flex flex-column justify-content-center align-items-center w-100 mx-auto py-3 text-white">' +
+                                '<div class="h5">' +
+                                '<span id="num">' + match.nom_partie + '</span>' +
+                                '<span id="type">' + match.nom_sport + '</span>' +
+                                '</div>' +
+                                '<div class="d-flex flex-row justify-content-around w-100 pt-4">' +
+                                '<div>' +
+                                '<div id="ville">' + match.ville + '</div>' +
+                                '<div id="date">' + match.date + '</div>' +
+                                '<div id="heure">' + match.heure + '</div>' +
+                                '</div>' +
+                                '<div>' +  
+                                '<div>Places : <span id="places">' + match.places_restantes + '</span></div>' +
+                                '<div>Joueurs inscrits : <span id="inscrits">' + match.nb_joueurs + '</span></div>' +
+                                '</div>' +
+                                '</div>' +
+                                '<div class="h6 text-marron">' +
+                                '<span id="joueur-orga">Organisateur</span>' +
                                 '</div>' +
                                 '</div>'
                                 );
--- code/php/requete.php
+++ code/php/requete.php
@@ -51,6 +51,15 @@ if ($requestMethod == 'POST')
         else
             $data = "nono";
     }
+
+    if ($requestRessource == 'filtreMesMatch')
+    {
+        
+        //if((isset($_POST['ville']) && isset($_POST['sport']) && isset($_POST['date']) && isset($_POST['dispo'])){
+            $data = dbRequestFiltreMesMatch($db, $_POST['sport'], $_POST['date'], $_POST['ville'], $_POST['dispo']);
+        //}
+    }
+
 }
 
 // Match request.
