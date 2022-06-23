5610209dd0edab94f9d9b78df5d778781a3f6c19
twist35 <sabironantonin@outlook.fr>
2022-06-23 16:52:13 +0200

    tt

--- code/js/creer-compte.js
+++ code/js/creer-compte.js
@@ -2,7 +2,7 @@
   Créé par Antonin SABIRON et Lucas Le Bihan
     le 15/06/2022
     Pour le projet de fin d'année CIR2
-    contient les fonctions pour l'authentification
+    contient les fonctions pour la creation compte
 */
 'use strict';
 
